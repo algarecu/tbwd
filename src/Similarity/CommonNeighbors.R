62b13e7116aef8b4b4b5e617cda5abf31b985aea
Alvaro Garcia-Recuero <algarecu@gmail.com>
2017-10-19 17:20:29 +0100

    Clustering based SNN communities with Louvain modularity

--- src/Similarity/CommonNeighbors.R
+++ src/Similarity/CommonNeighbors.R
@@ -1,6 +1,6 @@
 # Common Neighbors
 library(igraph)
-library(dbscan)
+library(reshape2)
 
 ##### Adjacency matrix #####
 
@@ -8,8 +8,7 @@ library(dbscan)
 cyberlockers <- read.csv("~/Documents/github/tbwd/datasets/similarity.csv", header=TRUE, sep=",")
 my_data <- cyberlockers
 
-library(reshape2)
-df <- acast(my_data, (my_data$SS1) ~ (my_data$SS2), value.var = "Similarity", fill=0, drop = FALSE)
+df <- acast(my_data, (my_data$SS1) ~ (my_data$SS2), value.var = "Similarity", fill=0, drop = TRUE)
 M<-as.matrix(df)
 
 # Create an edge IFF similarity > t.
@@ -17,7 +16,13 @@ M[M < 90] <- 0
 M[M > 90] <- 1
 diag(M) <- 0
 
-G = graph.adjacency(M, mode=c("undirected"),weighted=FALSE)
+# Remove the ones with no edge
+ind_row <- rowSums(M == 0) != ncol(M)
+ind_col <- colSums(M == 0) != nrow(M)
+
+Mt<-as.matrix(M[ind_row, ind_col])
+
+G = graph.adjacency(Mt, mode=c("undirected"))
 #Set minimum k<-2;number of shared neighbors in SNN graph
 k<-1
 # Calling SSN function
@@ -33,21 +38,25 @@ mylabels <- unique(melt(my_data$SS1, my_data$SS2))
 plot(G, layout=l,
      vertex.label=mylabels$value,
      vertex.label.color="black",
-     edge.curved=.1)
+     edge.curved=.3)
 plot(G2, layout=l,
      vertex.label=mylabels$value,
-     vertex.label.color="black")
-
+     vertex.label.color="black",
+     edge.curved=.3)
 
 # Modularity
-cfg <- cluster_fast_greedy(as.undirected(G))
+## Louvain
+cfg_louvain <- cluster_louvain(as.undirected(G), weights = NULL)
+## Newman: greedy modularity
+cfg_newman <- cluster_fast_greedy(as.undirected(G))
 
-pdf("~/Documents/github/tbwd/plots/community-html-modularity-all.pdf", width=8, height=8)
-pdfcommunity <- plot(cfg, as.undirected(G))
+pdf("~/Documents/github/tbwd/plots/community-html-modularity-all.pdf", width=6, height=6)
+pdfcommunity <- plot(cfg_louvain, as.undirected(G))
 
 dev.off()
 
 # SNN
+# library(dbscan)
 # cl <- sNNclust(M, k = 2, eps = 1, minPts = 0)
 # plot(M, col = cl$cluster + 1L, cex = .5)
 
