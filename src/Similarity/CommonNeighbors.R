1fe7bcc7a9e135cf18b34f2171f11e8c433c43a0
Alvaro Garcia-Recuero <algarecu@gmail.com>
2017-10-20 19:49:34 +0100

    Add column to Relationships datasets and rename filenames

--- src/Similarity/CommonNeighbors.R
+++ src/Similarity/CommonNeighbors.R
@@ -1,66 +1,106 @@
 # Common Neighbors
+# File: foo.R
+# Desc: Functionality related to foos.
+# Imports from other_resources.R
+
 library(igraph)
 library(reshape2)
+library(Matrix)
+library(RBGL)
+library(graph)
+
+# sapply(list.files(pattern="[.]R$", path="~/Documents/github/tbwd/src/Similarity/funcs/", full.names=TRUE), source);
+source("~/Documents/github/tbwd/src/Similarity/funcs/ProximityMeasure.R")
 
 ##### Adjacency matrix #####
 
 # Read cyberlocker sites to CSV
+networks <- read.csv("~/Documents/github/tbwd/datasets/networks.csv", header=TRUE, sep=",", as.is = T)
+domains <- read.csv("~/Documents/github/tbwd/datasets/domains.csv", header=TRUE, sep=",")
 cyberlockers <- read.csv("~/Documents/github/tbwd/datasets/similarity.csv", header=TRUE, sep=",")
-my_data <- cyberlockers
 
-df <- acast(my_data, (my_data$SS1) ~ (my_data$SS2), value.var = "Similarity", fill=0, drop = TRUE)
-M<-as.matrix(df)
+# mylabels <- unique(melt(my_data$SS1, my_data$SS2))
 
-# Create an edge IFF similarity > t.
-M[M < 90] <- 0
-M[M > 90] <- 1
-diag(M) <- 0
+# Create an edge iff similarity > t.
+# if(html_data != NULL) {
+#   df <- acast(html_data, (my_data$SS1) ~ (my_data$SS2), value.var = "Similarity", fill=0, drop = TRUE)
+#   Mt<-as.matrix(df)
+#   
+#   Mt[Mt < 90] <- 0
+#   Mt[Mt > 90] <- 1
+#   
+#   # Set diagonal back to zero
+#   diag(Mt) <- 0
+#   
+#   # Remove the ones with no edge
+#   ind_row <- rowSums(Mt == 0) != ncol(Mt)
+#   ind_col <- colSums(Mt == 0) != nrow(Mt)
+#   
+#   Mt<-as.matrix(Mt[ind_row, ind_col])
+# }
+#################################### LOUV ###################################
+nrow(networks)
+nrow(unique(networks[,c("source", "target")]))
+links <-as.matrix(networks[,c("source", "target")])
+dim(links)
+
+net <- graph_from_data_frame(d = networks, directed = FALSE)
+net
+
+E(net)
+V(net)
+
+G <-graph.edgelist(links)
+M <- get.adjacency(G)
+
+G1 = graph.adjacency(M, mode=c("undirected"))
+plot(G1, vertex.label.color="black", edge.curved=.1)
+
+cfg_louvain <- cluster_louvain(G1)
+pdf("~/Documents/github/tbwd/plots/community-networks-louvain.pdf", width=6, height=6)
+pdflouvain <- plot(cfg_louvain, G1)
+dev.off()
+
+#################################### SNN ####################################
+# Calling SSN function
+df <- acast(networks, (networks$source) ~ (networks$target), value.var = "link", fill=0, drop = TRUE)
+MSNN<-as.matrix(df)
+
+# Expand your matrix to a full-fledged adjacency matrix
+expand.matrix <- function(A){
+  m <- nrow(A)
+  n <- ncol(A)
+  B <- matrix(0,nrow = m, ncol = m)
+  C <- matrix(0,nrow = n, ncol = n)
+  cbind(rbind(B,t(A)),rbind(A,C))
+}
 
 # Remove the ones with no edge
-ind_row <- rowSums(M == 0) != ncol(M)
-ind_col <- colSums(M == 0) != nrow(M)
+ind_row <- rowSums(MSNN == 0) != ncol(MSNN)
+ind_col <- colSums(MSNN == 0) != nrow(MSNN)
 
-Mt<-as.matrix(M[ind_row, ind_col])
+MSNN2 <-as.matrix(MSNN[ind_row, ind_col])
 
-G = graph.adjacency(Mt, mode=c("undirected"))
-#Set minimum k<-2;number of shared neighbors in SNN graph
+G2 <- graph.adjacency(expand.matrix(MSNN2), mode="undirected")
+Madj <- get.adjacency(G2)
+
+# Calling SNN function and plotting the SNN graph returned by SNN GRAPH function.
 k<-1
-# Calling SSN function
-G2<-SNN_GRAPH(G,k);
-# Plot the SNN graph returned by SNN_GRAPH function
-tkid <- tkplot(G2,vertex.label.color="black", vertex.size=29)
+GNN <- SNN_GRAPH(Madj, k)
+tkid <- tkplot(GNN,vertex.label.color="black", vertex.size=29)
 l <- tkplot.getcoords(tkid)
 tk_close(tkid, window.close = T)
 
-mylabels <- unique(melt(my_data$SS1, my_data$SS2))
+# Plot the SNN graph returned by SNN_GRAPH function
+pdf("~/Documents/github/tbwd/plots/community-networks-snn.pdf", width=6, height=6)
 
-
-plot(G, layout=l,
-     vertex.label=mylabels$value,
-     vertex.label.color="black",
-     edge.curved=.3)
-plot(G2, layout=l,
-     vertex.label=mylabels$value,
-     vertex.label.color="black",
-     edge.curved=.3)
-
-# Modularity
-## Louvain
-cfg_louvain <- cluster_louvain(as.undirected(G), weights = NULL)
-## Newman: greedy modularity
-cfg_newman <- cluster_fast_greedy(as.undirected(G))
-
-pdf("~/Documents/github/tbwd/plots/community-html-modularity-all.pdf", width=6, height=6)
-pdfcommunity <- plot(cfg_louvain, as.undirected(G))
-
+cfg_louvain_snn <- cluster_louvain(GNN)
+pdfsnn <- plot(cfg_louvain_snn, GNN)
 dev.off()
 
-# SNN
-# library(dbscan)
-# cl <- sNNclust(M, k = 2, eps = 1, minPts = 0)
-# plot(M, col = cl$cluster + 1L, cex = .5)
-
-# KNN
-# nn <- kNN(M, k=1)
-# nn
-# plot(nn, M)
+# G = graph.data.frame(d = as.data.frame(MSNN), directed = FALSE)
+# Output = SNN_Clustering(G,3)
+# Output
+#################################### END ####################################
+# Newman: greedy modularity
+# cfg_newman <- cluster_fast_greedy(as.undirected(G2))
--- src/crawler.py
+++ src/crawler.py
@@ -4,10 +4,11 @@
 import pandas as pd
 import requests
 import json
+import base64
 
 df=pd.read_csv('../datasets/1-ss-hs-is-metadata.csv', usecols=['STREAMDOMAIN'])
 
-print "The list of straming domains \n"
+print "The list of streaming domains \n"
 print list(df.apply(set)[0])
 
 for item in list(df.apply(set)[0]):
@@ -20,11 +21,16 @@ for item in list(df.apply(set)[0]):
                 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     # Parse
     r = requests.get(url, headers=headers)
+    # body = json.loads(r.text)
 
     # Check status_code and write to file
     if r.status_code == requests.codes.ok:
         print "Correct URL, alive and parsed"
-        # Save content to text file
-        with open('../datasets/html/' + item + '.html', 'wb') as fd:
+        # Save website html and image files
+        with open('../datasets/html/' + item, 'wb') as fd:
             for chunk in r.iter_content(chunk_size=128):
                 fd.write(chunk)
+        # with open('../datasets/png/%s.png' % item, 'wb') as pngfile:
+        #     pngfile.write(base64.b64decode(body['png']))
+    else:
+        print("Request to %s failed with error code %d" % (item, r.status_code) )
