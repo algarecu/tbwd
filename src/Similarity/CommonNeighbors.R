d3247b3b9143b113097d12e4a5ad1f61167d1f99
Alvaro Garcia-Recuero <algarecu@gmail.com>
2017-10-21 22:44:05 +0100

    Plots working with Louvain and SNN

--- src/Similarity/CommonNeighbors.R
+++ src/Similarity/CommonNeighbors.R
@@ -8,22 +8,21 @@ library(reshape2)
 library(Matrix)
 library(RBGL)
 library(graph)
-library(tkplot)
 
 # sapply(list.files(pattern="[.]R$", path="~/Documents/github/tbwd/src/Similarity/funcs/", full.names=TRUE), source);
 source("~/Documents/github/tbwd/src/Similarity/funcs/ProximityMeasure.R")
 
 # Read cyberlocker sites to CSV
 networks <- read.csv("~/Documents/github/tbwd/datasets/networks.csv", header=TRUE, sep=",", as.is = T)
-domains <- read.csv("~/Documents/github/tbwd/datasets/domains.csv", header=TRUE, sep=",")
-html <- read.csv("~/Documents/github/tbwd/datasets/html-t90.csv", header=TRUE, sep=",")
+domains <- read.csv("~/Documents/github/tbwd/datasets/domains.csv", header=TRUE, sep=",", as.is = T)
+html <- read.csv("~/Documents/github/tbwd/datasets/html-t90.csv", header=TRUE, sep=",", as.is = T)
 
 # nrow(unique(domains[,c("source", "target")]))
 #################################### GRAPH ###################################
 # Converting to format to call SSN_GRAPH function
-df_domains <- acast(domains, (domains$source) ~ (domains$target), value.var = "link", fill=0, drop = TRUE)
-df_networks <- acast(domains, (domains$source) ~ (domains$target), value.var = "link", fill=0, drop = TRUE)
-df_html <- acast(domains, (domains$source) ~ (domains$target), value.var = "link", fill=0, drop = TRUE)
+df_domains <- acast(domains, (domains$source) ~ (domains$target), value.var = "link", fill=0)
+df_networks <- acast(networks, (networks$source) ~ (networks$target), value.var = "link", fill=0)
+df_html <- acast(html, (html$source) ~ (html$target), value.var = "link", fill=0)
 
 links_domains <-as.matrix(domains[,c("source", "target")])
 links_networks <-as.matrix(networks[,c("source", "target")])
@@ -60,27 +59,22 @@ expand.matrix <- function(A){
 ind_row <- rowSums(MSNN_domains == 0) != ncol(MSNN_domains)
 ind_col <- colSums(MSNN_domains == 0) != nrow(MSNN_domains)
 
-MSNN2 <-as.matrix(MSNN_domains[ind_row, ind_col])
-G2 <- graph.adjacency(expand.matrix(MSNN2), mode="undirected")
-Madj <- get.adjacency(G2)
+# MSNN2 <-as.matrix(MSNN_domains[ind_row, ind_col])
+G2_domains <- graph.adjacency(expand.matrix(MSNN_domains), mode="undirected")
+Madj <- get.adjacency(G2_domains)
 
 # Calling SNN function and plotting the SNN graph returned by SNN GRAPH function.
-k<-2
+k<-1
 GNN_domains <- SNN_GRAPH(Madj, k)
-
-# Take care of layout
-tkid <- tkplot(GNN_domains,vertex.label=domains_labels, vertex.label.color="black", vertex.size=29)
-l <- tkplot.getcoords(tkid)
-tk_close(tkid, window.close = T)
 
 # Plot the SNN graph returned by SNN_GRAPH
 pdf("~/Documents/github/tbwd/plots/community-domains-snn.pdf", width=10, height=10)
-pdfsnn <- plot(GNN_domains, vertex.label.color="black", vertex.label=domains_labels$value,)
+pdfdomainssnn <- plot(GNN_domains, vertex.label=domains_labels$value, vertex.label.color="black", edge.curved=.3)
 dev.off()
 
 domains_louvain <- cluster_louvain(G1_domains)
 pdf("~/Documents/github/tbwd/plots/community-domains-louvain.pdf", width=10, height=10)
-domainslouvain <- plot(domains_louvain, G1_domains)
+domainslouvain <- plot(domains_louvain, G1_domains, vertex.label.color="black", edge.curved=.3)
 dev.off()
 
 #################################### END DOMAINS ###############################
@@ -92,37 +86,23 @@ G1_networks = graph.adjacency(M_networks, mode=c("undirected"))
 plot(G1_networks, vertex.label.color="black", edge.curved=.1)
 
 # Matrix for networks
-MSNN_networks<-as.matrix(df_networks)
+MSNN_networks <- as.matrix(df_networks)
 
-# Labels for domains
+# Labels for networks
 networks_labels <- unique(melt(networks$source, networks$target))
 
 # Calling SNN function and plotting the SNN graph returned by SNN GRAPH function.
 k<-1
 GNN_networks <- SNN_GRAPH(MSNN_networks, k)
 
-# Take care of layout
-tkid <- tkplot(GNN_networks,vertex.label=networks_labels, vertex.label.color="black", vertex.size=29)
-l <- tkplot.getcoords(tkid)
-tk_close(tkid, window.close = T)
-
 # Plot the SNN graph returned by SNN_GRAPH
-pdf("~/Documents/github/tbwd/plots/community-html-snn.pdf", width=10, height=10)
-pdfsnn <- plot(GNN_networks, layout=l, 
-               vertex.label=networks_labels,
-               vertex.label.color="black")
-
-dev.off()
-
-# Plot the SNN graph returned by SNN_GRAPH with Louvain function
-pdf("~/Documents/github/tbwd/plots/community-networks-snn-louvain.pdf", width=10, height=10)
-networks_louvain_snn <- cluster_louvain(GNN_networks)
-networks_snnlouvain <- plot(networks_louvain_snn, GNN_networks)
+pdf("~/Documents/github/tbwd/plots/community-networks-snn.pdf", width=10, height=10)
+networks_snn <- plot(GNN_networks, vertex.label=networks_labels$value, vertex.label.color="black", edge.curved=.3)
 dev.off()
 
 networks_louvain <- cluster_louvain(G1_networks)
 pdf("~/Documents/github/tbwd/plots/community-networks-louvain.pdf", width=10, height=10)
-networkslouvain <- plot(networks_louvain, G1_networks)
+networks_louvain <- plot(networks_louvain, G1_networks,vertex.label.color="black", edge.curved=.3)
 dev.off()
 
 #################################### END NETWORK ###############################
@@ -134,12 +114,9 @@ G1_html = graph.adjacency(M_html, mode=c("undirected"))
 plot(G1_html, vertex.label.color="black", edge.curved=.1)
 
 # Matrix for html
-MSNN_html<-as.matrix(df)
+MSNN_html<-as.matrix(df_html)
 
-# Labels for domains
-networks_labels <- unique(melt(networks$source, networks$target))
-
-# Labels for Html
+# Labels for html
 html_labels <- unique(melt(html$source, html$target))
 
 MSNN_html[MSNN_html < 90] <- 0
@@ -155,21 +132,16 @@ ind_col <- colSums(MSNN_html == 0) != nrow(MSNN_html)
 MSNN_html<-as.matrix(MSNN_html[ind_row, ind_col])
 
 # Calling SNN function and plotting the SNN graph returned by SNN GRAPH function.
-k<-2
+k<-1
 GNN_html <- SNN_GRAPH(MSNN_html, k)
 
-# Take care of layout
-tkid <- tkplot(GNN_html,vertex.label=html_labels, vertex.label.color="black", vertex.size=29)
-l <- tkplot.getcoords(tkid)
-tk_close(tkid, window.close = T)
-
 # Plot the SNN graph returned by SNN_GRAPH
-pdf("~/Documents/github/tbwd/plots/community-domains-snn.pdf", width=10, height=10)
-pdfsnn <- plot(GNN_html, vertex.label.color="black", vertex.label=domains_labels$value,)
+pdf("~/Documents/github/tbwd/plots/community-html-t90-snn.pdf", width=10, height=10)
+pdfsnn <- plot(GNN_html, vertex.label.color="black", vertex.label=html_labels$value)
 dev.off()
 
 html_louvain <- cluster_louvain(G1_html)
-pdf("~/Documents/github/tbwd/plots/community-html-louvain.pdf", width=10, height=10)
+pdf("~/Documents/github/tbwd/plots/community-html-t90-louvain.pdf", width=10, height=10)
 htmllouvain <- plot(html_louvain, G1_html)
 dev.off()
 
